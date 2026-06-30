2e0b7862137847d904055eee0b93d86fe2a62f4c
Swati Singh <swatisingh022805@gmail.com>
2026-06-30 12:29:09 +0530

    using loops( if else and for)

---
+++ src/app/component/controlflow/controlflow.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-controlflow',
+  imports: [],
+  templateUrl: './controlflow.html',
+  styleUrl: './controlflow.css',
+})
+export class Controlflow {}
